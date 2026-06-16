1b09ce077a164710fdc8d59f4c69aa25028b3294
harinsd404 <harinsd168@gmail.com>
2026-06-17 02:11:12 +0900

    feat:: TextInput 컴포넌트 추가

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -1,11 +1,16 @@
 import BackButton from '@/components/ui/BackButton';
-import { Text, View } from 'react-native';
+import TextInput from '@/components/ui/input/TextInput';
+import Typography from '@/components/ui/Typography';
+import { View } from 'react-native';
 
 const SignUp = () => {
   return (
     <View className="flex-1 items-center justify-center">
       <BackButton title="정보등록" link={'/login'} />
-      <Text className="text-xl font-bold text-gray-900">회원 정보 등록</Text>
+      <Typography variant="display" className="text-gray-900">
+        회원 정보 등록
+      </Typography>
+      <TextInput label="닉네임" placeholder="닉네임을 입력해주세요" />
     </View>
   );
 };
