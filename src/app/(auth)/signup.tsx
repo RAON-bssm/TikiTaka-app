6f2ab722d7a4b098ee993f5127e53d658e29a101
harinsd404 <harinsd168@gmail.com>
2026-06-17 13:20:48 +0900

    feat:: 버튼 컴포넌트 추가

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -6,25 +6,29 @@ import { View } from 'react-native';
 
 const SignUp = () => {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex flex-col flex-1 items-start justify-start p-lg gap-4xl">
       <BackButton title="정보등록" link={'/login'} />
       <Typography variant="display" className="text-gray-900">
         회원 정보 등록
       </Typography>
-      <TextInput label="닉네임" placeholder="닉네임을 입력해주세요" />
-      <View className="flex flex-row gap-sm items-end w-full">
-        <View className="flex-1">
-          <Dropdown
-            label="동네"
-            placeholder="시/도"
-            options={['부산시', '서울시', '대구시', '인천시', '광주시', '대전시', '울산시']}
-          />
+      <View className="flex flex-col gap-3xl w-full">
+        <View>
+          <TextInput label="닉네임" placeholder="닉네임을 입력해주세요" />
         </View>
-        <View className="flex-1">
-          <Dropdown
-            placeholder="구/군"
-            options={['영도구', '사상구', '기장군', '연제구', '강서구', '수영구', '해운대구']}
-          />
+        <View className="flex flex-row gap-sm items-end w-full">
+          <View className="flex-1">
+            <Dropdown
+              label="동네"
+              placeholder="시/도"
+              options={['부산시', '서울시', '대구시', '인천시', '광주시', '대전시', '울산시']}
+            />
+          </View>
+          <View className="flex-1">
+            <Dropdown
+              placeholder="구/군"
+              options={['영도구', '사상구', '기장군', '연제구', '강서구', '수영구', '해운대구']}
+            />
+          </View>
         </View>
       </View>
     </View>
