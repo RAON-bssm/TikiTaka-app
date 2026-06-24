2b56174e4c8a57390f029fc92524c8fe6e89dc93
harinsd404 <harinsd168@gmail.com>
2026-06-25 01:47:47 +0900

    feat:: 피드 미리보기 카드 컴포넌트 구현

--- src/components/feed/FeedPreviewCard.tsx
+++ src/components/feed/FeedPreviewCard.tsx
@@ -1,9 +1,39 @@
-import { View } from 'react-native';
+import PlaceIcon from '@/assets/icons/place.svg';
+import { Image, ImageBackground, View } from 'react-native';
+import Typography from '../ui/Typography';
 
-const FeedPreviewCard = () => {
+interface Props {
+  imgUrl: string;
+  user: {
+    name: string;
+    profile: string;
+  };
+  place: string;
+}
+
+const FeedPreviewCard = ({ imgUrl, user, place }: Props) => {
   return (
-    <View>
-      <View></View>
-    </View>
+    <ImageBackground
+      source={{ uri: imgUrl }}
+      className="w-[160px] h-[220px] overflow-hidden rounded-md"
+    >
+      <View className="absolute inset-0 bg-black/20" />
+      <View className="flex-1 justify-between p-md">
+        <View className="flex flex-row items-center gap-xs">
+          <Image source={{ uri: user.profile }} className="w-8 h-8 rounded-full" />
+          <Typography variant="h4" className="text-gray-50">
+            {user.name}
+          </Typography>
+        </View>
+        <View className="flex flex-row items-center gap-xs">
+          <PlaceIcon width={16} height={16} color="#F9FAFB" />
+          <Typography variant="h4" className="text-gray-50">
+            {place}
+          </Typography>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
+
+export default FeedPreviewCard;
