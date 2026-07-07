6ba563136f9381d7f358293c70e896255ac9215e
harinsd404 <harinsd168@gmail.com>
2026-07-07 15:05:28 +0900

    feat:: 헤어 파일 추가

--- src/constants/character/assets.ts
+++ src/constants/character/assets.ts
@@ -88,6 +88,11 @@ const COLOR_ASSETS: Record<ColorGroup, Record<string, Record<string, number>>> =
       blond: require('@/assets/character/hair-back/low-tail/blond.webp'),
       brown: require('@/assets/character/hair-back/low-tail/brown.webp'),
     },
+    'low-pigtails': {
+      black: require('@/assets/character/hair-back/low-pigtails/black.webp'),
+      blond: require('@/assets/character/hair-back/low-pigtails/blond.webp'),
+      brown: require('@/assets/character/hair-back/low-pigtails/brown.webp'),
+    },
     'side-wave': {
       black: require('@/assets/character/hair-back/side-wave/black.webp'),
       blond: require('@/assets/character/hair-back/side-wave/blond.webp'),
